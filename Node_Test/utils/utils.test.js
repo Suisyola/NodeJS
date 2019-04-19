b6b19f07380362d3f3bbdb544b8b7774dc9cdd12
Aloysius <tics_zz@hotmail.com>
2019-04-19 15:04:39 +0800

    Test
    Writing test case with Mocha

---
+++ Node_Test/utils/utils.test.js
@@ -0,0 +1,18 @@
+const utils = require('./utils.js');
+
+// Define new test case
+it('should add two numbers', () => {
+    var result = utils.add(33, 11);
+
+    if (result !== 44){
+        throw new Error(`Expected 44, but got ${result}.`);
+    }
+});
+
+it('should square a number', () => {
+    var result = utils.square(3);
+
+    if (result !== 9){
+        throw new Error(`Expected 9, but got ${result}.`);
+    }
+});
