0e6e5b43f501964d3acc9390027fd2d925082626
Aloysius <tics_zz@hotmail.com>
2019-04-20 12:50:30 +0800

    Asynchronous Testing

--- Node_Test/utils/utils.test.js
+++ Node_Test/utils/utils.test.js
@@ -12,6 +12,15 @@ it('should add two numbers', () => {
     expect(result).toBe(44).toBeA('number');
 });
 
+// the done argument tells Mocha that this is a async test
+// Mocha would not process the assertion until done gets called.
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    })
+});
+
 it('should square a number', () => {
     var result = utils.square(3);
 
@@ -20,6 +29,13 @@ it('should square a number', () => {
     // }
 
     expect(result).toBe(9).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+    utils.asyncSquare(4, (result) => {
+        expect(result).toBe(16).toBeA('number');
+        done();
+    })
 });
 
 it('should verify that first and last names are set', () => {
