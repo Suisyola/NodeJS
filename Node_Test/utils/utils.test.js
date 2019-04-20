7d3e4b67be8d4a055ccc9e9a5a4ccab28c5d1ab4
Aloysius <tics_zz@hotmail.com>
2019-04-20 12:14:24 +0800

    Testing using Assertion Library
    Using Expect to simplify test cases

--- Node_Test/utils/utils.test.js
+++ Node_Test/utils/utils.test.js
@@ -1,18 +1,39 @@
 const utils = require('./utils.js');
+const expect = require('expect');
 
 // Define new test case
 it('should add two numbers', () => {
     var result = utils.add(33, 11);
 
-    if (result !== 44){
-        throw new Error(`Expected 44, but got ${result}.`);
-    }
+    // if (result !== 44){
+    //     throw new Error(`Expected 44, but got ${result}.`);
+    // }
+
+    expect(result).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var result = utils.square(3);
 
-    if (result !== 9){
-        throw new Error(`Expected 9, but got ${result}.`);
-    }
+    // if (result !== 9){
+    //     throw new Error(`Expected 9, but got ${result}.`);
+    // }
+
+    expect(result).toBe(9).toBeA('number');
+});
+
+it('should verify that first and last names are set', () => {
+    var user = {
+        age: 24,
+        fullName: 'Aloysius Lim'
+    };
+
+    var result = utils.setName(user, user.fullName);
+
+    expect(result).toBeA('object')
+    .toInclude({
+        firstName: 'Aloysius'
+    }).toInclude({
+        lastName: 'Lim'
+    });
 });
