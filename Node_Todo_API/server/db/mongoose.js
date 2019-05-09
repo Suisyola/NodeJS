d5db794084c3806ea22343a6c181fcf8cbc42793
Aloysius <tics_zz@hotmail.com>
2019-05-09 22:23:02 +0800

    API (Add DELETE /todos:id route)

--- Node_Todo_API/server/db/mongoose.js
+++ Node_Todo_API/server/db/mongoose.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 // Use the native Promise instead of Mongoose's implementation of Promise
 mongoose.Promise = global.Promise;
 
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb+srv://admin:P@ssw0rd@cluster0-n7vld.mongodb.net/test?retryWrites=true');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
 
 module.exports = {
     mongoose: mongoose
