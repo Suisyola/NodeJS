7027acb3b3e7cf40042362043d7f17b6c75b6c0e
Aloysius <tics_zz@hotmail.com>
2019-05-10 14:06:41 +0800

    Added compatibility to host on Heroku and MongoDB:Atlas

--- Node_Todo_API/server/config/config.js
+++ Node_Todo_API/server/config/config.js
@@ -3,8 +3,11 @@ console.log(`Environment is : ${env}` );
 
 if (env === 'development') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    //process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.MONGODB_URI = 'mongodb+srv://admin:jiayou@cluster0-n7vld.mongodb.net/Node_Todo_API?retryWrites=true';
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if (env === 'production') {
+    process.env.MONGODB_URI = 'mongodb+srv://admin:jiayou@cluster0-n7vld.mongodb.net/Node_Todo_API?retryWrites=true';
 }
