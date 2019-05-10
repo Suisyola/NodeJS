7c32149782930781173c5ec551e2c030e4d323d8
Aloysius <tics_zz@hotmail.com>
2019-05-10 14:10:59 +0800

    Modified Dev environment to use local DB

--- Node_Todo_API/server/config/config.js
+++ Node_Todo_API/server/config/config.js
@@ -3,8 +3,7 @@ console.log(`Environment is : ${env}` );
 
 if (env === 'development') {
     process.env.PORT = 3000;
-    //process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-    process.env.MONGODB_URI = 'mongodb+srv://admin:jiayou@cluster0-n7vld.mongodb.net/Node_Todo_API?retryWrites=true';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
