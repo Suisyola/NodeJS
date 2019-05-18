3e1ff075c99f843ce39e7b224523432959083203
Aloysius <tics_zz@hotmail.com>
2019-05-18 13:32:48 +0800

    Add POST users/login route

--- Node_Todo_API/server/models/user.js
+++ Node_Todo_API/server/models/user.js
@@ -79,6 +79,30 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({'email': email}).then((user) => {
+        if (!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(err) {
+                    // This would trigger the catch() in Promise then..catch()
+                    reject();
+                }
+
+                if (res){
+                    resolve(user);
+                }
+                
+            });
+        });
+    })
+};
+
 // Callback function is called before(pre) UserSchema's save event
 UserSchema.pre('save', function(next){
     var user = this;
