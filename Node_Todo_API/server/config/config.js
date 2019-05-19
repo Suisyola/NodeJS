65e84e4e061203650475c7139fd4c6641c5b7606
Aloysius <tics_zz@hotmail.com>
2019-05-19 23:46:45 +0800

    Update config to use JSON file

--- Node_Todo_API/server/config/config.js
+++ Node_Todo_API/server/config/config.js
@@ -1,12 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 console.log(`Environment is : ${env}` );
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if (env === 'production') {
-    process.env.MONGODB_URI = 'mongodb+srv://admin:jiayou@cluster0-n7vld.mongodb.net/Node_Todo_API?retryWrites=true';
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];    // need to use bracket notation, [env], when we want to use variable to access JSON property
+
+    // .key() parse the JSON properties into array of key
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];      //e.g. process.env.PORT = envConfig.PORT
+    });
 }
