8e176c068785089f35bcf06ac09227b21cf37c8a
Aloysius <tics_zz@hotmail.com>
2019-07-10 21:34:30 +0800

    Send messages to only people in the same room

--- Node_Chat_App/server/server.js
+++ Node_Chat_App/server/server.js
@@ -51,16 +51,23 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (newMessage, callback) => {
-        console.log('createMessage', newMessage);
+        var user = users.getUser(socket.id);
 
-        // io.emit emit event to all connections
-        io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+        if (user && isRealString(newMessage.text)) {
+            // io.emit emit event to all connections
+            io.to(user.room).emit('newMessage', generateMessage(user.name, newMessage.text));
+        }
 
         callback('This is from the server');
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude, coords.longitude));
+        var user = users.getUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name,coords.latitude, coords.longitude));
+        }
+
     });
 
     // listen for client that disconnect
